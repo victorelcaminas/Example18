526f77f3b0dcb62c874732208c7be6c7bbb92c6c
Victor Alonso <victor@ieselcaminas.org>
2020-10-14 00:06:29 +0200

    Indentation + spaces

--- src/Example18.java
+++ src/Example18.java
@@ -19,31 +19,22 @@ class Example18 {
 
         if (workHours < 35) {
             grossPay = payment*workHours;
-
-        }
-        else {
+        }  else {
             grossPay = 35 * payment;
-
             double extraPaymentTax = payment * 1.5;
-
             double extraPayment = (workHours - 35) * extraPaymentTax;
-
             grossPay = grossPay + extraPayment;
         }
         double netPay,taxes;
 
         if (grossPay < 500) {
             taxes = 0;
-        }
-        else if (grossPay < 900 ) {
-            taxes = (grossPay-500)*0.25;
-
-        }
-
-        else {
+        } else if (grossPay < 900 ) {
+            taxes = (grossPay - 500) * 0.25;
+        } else {
             taxes = 400 * 0.25 + (grossPay - 900) * 0.45;
         }
-         netPay = grossPay - taxes;
+        netPay = grossPay - taxes;
 
         System.out.println("Your gross payment is " + grossPay);
         System.out.println("Your net payment is " + netPay);
