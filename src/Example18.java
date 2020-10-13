885499786aeb4c0aa179896916d49ad0a7969b29
Victor Alonso <victor@ieselcaminas.org>
2020-10-14 00:09:58 +0200

    Indentation and spaces

--- src/Example18.java
+++ src/Example18.java
@@ -3,47 +3,38 @@ class Example18 {
     public static void main (String [] argv) {
          int payment, workHours;
 
-        System.out.println("Enter price per hour in €");
-
-        System.out.println("Enter hours that you work");
-
         //Reading value
         Scanner inputValue;
         inputValue = new Scanner(System.in);
+        System.out.println("Enter price per hour in €");
         payment = inputValue.nextInt();
 
         inputValue = new Scanner(System.in);
+        System.out.println("Enter hours that you work");
         workHours = inputValue.nextInt();
 
         double grossPay;
 
         if (workHours < 35) {
             grossPay = payment*workHours;
-
-        }
-        else {
+        } else {
             grossPay = 35 * payment;
-
             double extraPaymentTax = payment * 1.5;
-
             double extraPayment = (workHours - 35) * extraPaymentTax;
-
             grossPay = grossPay + extraPayment;
         }
+        
         double netPay,taxes;
 
         if (grossPay < 500) {
             taxes = 0;
-        }
-        else if (grossPay < 900 ) {
+        } else if (grossPay < 900 ) {
             taxes = (grossPay-500)*0.25;
-
-        }
-
-        else {
+        } else {
             taxes = 400 * 0.25 + (grossPay - 900) * 0.45;
         }
-         netPay = grossPay - taxes;
+        
+        netPay = grossPay - taxes;
 
         System.out.println("Your gross payment is " + grossPay);
         System.out.println("Your net payment is " + netPay);
